9b01c99fee8bc47ba3eab941a8e895ba6ae72bff
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-12-29 02:27:06 -0500

    digits enum

---
+++ src/main/java/com/two_weeks_backend/two_weeks_backend/entities/management/Digits.java
@@ -0,0 +1,25 @@
+package com.two_weeks_backend.two_weeks_backend.entities.management;
+
+public enum Digits {
+    TWO(2),
+    THREE(3);
+
+    private int value;
+
+    Digits(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public static Digits fromString(int value) {
+        for (Digits digits : Digits.values()) {
+            if (digits.value == value) {
+                return digits;
+            }
+        }
+        throw new IllegalArgumentException("Digito desconocido:  " + value);
+    }
+}
