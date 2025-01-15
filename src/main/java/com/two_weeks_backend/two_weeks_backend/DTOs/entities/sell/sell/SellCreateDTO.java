a06c27c74133a5d5114d096de7308214446f2827
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-01-15 02:10:48 -0500

    sell create dto finished

--- src/main/java/com/two_weeks_backend/two_weeks_backend/DTOs/entities/sell/sell/SellCreateDTO.java
+++ src/main/java/com/two_weeks_backend/two_weeks_backend/DTOs/entities/sell/sell/SellCreateDTO.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import com.two_weeks_backend.two_weeks_backend.DTOs.entities.sell.sell_detail.SellDetailCreateDTO;
 import com.two_weeks_backend.two_weeks_backend.DTOs.entities.sell.sell_pay_method.SellPayMethodCreateDTO;
+import com.two_weeks_backend.two_weeks_backend.entities.company.Company;
 import com.two_weeks_backend.two_weeks_backend.entities.sell.DocumentType;
 import com.two_weeks_backend.two_weeks_backend.entities.sell.Sell;
+import com.two_weeks_backend.two_weeks_backend.entities.tenant.Tenant;
+import com.two_weeks_backend.two_weeks_backend.entities.user.User;
 
 import jakarta.validation.constraints.*;
 import lombok.*;
@@ -43,7 +46,46 @@ public class SellCreateDTO {
 
     public Sell asEntity() {
         Sell sell = new Sell();
-        
+
+        String clientName = this.getClientName() != null ? this.getClientName().trim() : null;
+        String clientDNI = this.getClientDNI() != null ? this.getClientDNI().trim() : null;
+        String clientRUC = this.getClientRUC() != null ? this.getClientRUC().trim() : null;
+
+        sell.setDate(this.getDate());
+        sell.setClientName(clientName);
+        sell.setClientDNI(clientDNI);
+        sell.setClientRUC(clientRUC);
+        sell.setDocumentType(this.getDocumentType());
+        sell.setSeller(this.getSellerEntity());
+        sell.setTenant(this.getTenantEntity());
+        sell.setCompany(this.getCompanyEntity());
         return sell;
+    }
+
+    private User getSellerEntity() {
+        if (this.getSellerId() != null) {
+            User user = new User();
+            user.setId(this.getSellerId());
+            return user;
+        }
+        return null;
+    }
+
+    private Tenant getTenantEntity() {
+        if (this.getTenantId() != null) {
+            Tenant tenant = new Tenant();
+            tenant.setId(this.getTenantId());
+            return tenant;
+        }
+        return null;
+    }
+
+    private Company getCompanyEntity() {
+        if (this.getCompanyId() != null) {
+            Company company = new Company();
+            company.setId(this.getCompanyId());
+            return company;
+        }
+        return null;
     }
 }
