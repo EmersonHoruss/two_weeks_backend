06efb93c00f1edc8496e804ae0b41221863adac9
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-01-15 01:44:56 -0500

    doing the sell process, saved for precaution: finished creation of sell details

--- src/main/java/com/two_weeks_backend/two_weeks_backend/services/sell/SellDetailService.java
+++ src/main/java/com/two_weeks_backend/two_weeks_backend/services/sell/SellDetailService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.two_weeks_backend.two_weeks_backend.DTOs.entities.sell.sell_detail.SellDetailCreateDTO;
+import com.two_weeks_backend.two_weeks_backend.entities.catalog.PriceLimit;
 import com.two_weeks_backend.two_weeks_backend.entities.catalog.PriceType;
 import com.two_weeks_backend.two_weeks_backend.entities.catalog.Product;
 import com.two_weeks_backend.two_weeks_backend.entities.company.ProductCompany;
@@ -58,18 +59,52 @@ public class SellDetailService {
         if (priceType == PriceType.AUCTION || priceType == PriceType.PREFERENCE)
             return;
 
+        if (priceType == PriceType.NORMAL)
+            this.validateNormalPrice(sellDetailDTO, product);
+
+        if (priceType == PriceType.WHOLESALE)
+            this.validateWholesalePrice(sellDetailDTO, product);
+
+        if (priceType == PriceType.SUPER_WHOLESAE)
+            this.validateSuperWholesalePrice(sellDetailDTO, product);
+    }
+
+    private void validateNormalPrice(SellDetailCreateDTO sellDetailDTO, Product product) {
         float subTotal = sellDetailDTO.getSubTotal();
-        if (priceType == PriceType.NORMAL) this.validateNormalPrice();
+        int amount = sellDetailDTO.getAmount();
 
-        if(priceType==PriceType.)
+        if (!(amount >= PriceLimit.NORMAL_FROM.getValue() && amount <= PriceLimit.NORMAL_TO.getValue()))
+            throw new RuntimeException("El tipo de precio normal debe tener una cantidad entre: "
+                    + PriceLimit.NORMAL_FROM + " hasta " + PriceLimit.NORMAL_TO);
+
+        if (!(product.getSellPriceNormal() * amount == subTotal))
+            throw new RuntimeException(
+                    "El subtotal no concuerda con el tipo de precio normal y la cantidad del producto");
     }
 
-    private void validateNormalPrice() {
+    private void validateWholesalePrice(SellDetailCreateDTO sellDetailDTO, Product product) {
+        float subTotal = sellDetailDTO.getSubTotal();
+        int amount = sellDetailDTO.getAmount();
+
+        if (!(amount >= PriceLimit.WHOLESALE_FROM.getValue() && amount <= PriceLimit.WHOLESALE_TO.getValue()))
+            throw new RuntimeException("El tipo de precio al por mayor debe tener una cantidad entre: "
+                    + PriceLimit.WHOLESALE_FROM + " hasta " + PriceLimit.WHOLESALE_TO);
+
+        if (!(product.getSellPriceWholesale() * amount == subTotal))
+            throw new RuntimeException(
+                    "El subtotal no concuerda con el tipo de precio al por mayor y la cantidad del producto");
     }
 
-    private void validateWholesalePrice() {
-    }
+    private void validateSuperWholesalePrice(SellDetailCreateDTO sellDetailDTO, Product product) {
+        float subTotal = sellDetailDTO.getSubTotal();
+        int amount = sellDetailDTO.getAmount();
 
-    private void validateSuperWholesalePrice() {
+        if (!(amount >= PriceLimit.SUPER_WHOLESAE_FROM.getValue()))
+            throw new RuntimeException("El tipo de precio super mayor debe tener una cantidad entre: "
+                    + PriceLimit.SUPER_WHOLESAE_FROM + " hasta todos los que existan");
+
+        if (!(product.getSellPriceWholesaleSuper() * amount == subTotal))
+            throw new RuntimeException(
+                    "El subtotal no concuerda con el tipo de precio al súper por mayor y la cantidad del producto");
     }
 }
