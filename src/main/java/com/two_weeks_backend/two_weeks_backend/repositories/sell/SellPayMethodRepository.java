10f8a2e42566685481fb942eba1938034b53a7bf
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-01-18 12:23:14 -0500

    repositories with inherintace of base repository for sell change detail, sell cahnge, sell detail, sell pay method, sell returning, and sell returning detail

--- src/main/java/com/two_weeks_backend/two_weeks_backend/repositories/sell/SellPayMethodRepository.java
+++ src/main/java/com/two_weeks_backend/two_weeks_backend/repositories/sell/SellPayMethodRepository.java
@@ -1,11 +1,11 @@
 package com.two_weeks_backend.two_weeks_backend.repositories.sell;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.two_weeks_backend.two_weeks_backend.entities.sell.SellPayMethod;
+import com.two_weeks_backend.two_weeks_backend.repositories.BaseRepository;
 
 @Repository
-public interface SellPayMethodRepository extends JpaRepository<SellPayMethod, Long> {
+public interface SellPayMethodRepository extends BaseRepository<SellPayMethod> {
 
 }
--- src/main/java/com/two_weeks_backend/two_weeks_backend/repositories/sell/SellReturningRepository.java
+++ src/main/java/com/two_weeks_backend/two_weeks_backend/repositories/sell/SellReturningRepository.java
@@ -2,10 +2,10 @@ package com.two_weeks_backend.two_weeks_backend.repositories.sell;
 
 import org.springframework.stereotype.Repository;
 
-import com.two_weeks_backend.two_weeks_backend.entities.sell.SellDetail;
+import com.two_weeks_backend.two_weeks_backend.entities.sell.SellReturning;
 import com.two_weeks_backend.two_weeks_backend.repositories.BaseRepository;
 
 @Repository
-public interface SellDetailRepository extends BaseRepository<SellDetail> {
+public interface SellReturningRepository extends BaseRepository<SellReturning> {
 
 }
