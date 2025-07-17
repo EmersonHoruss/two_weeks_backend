3dfc55d087be95c4216b7fa2b59f27eb3fba5973
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-07-17 00:01:30 -0500

    caja format and fixed mutation returnable value

--- src/main/java/com/two_weeks_backend/two_weeks_backend/repositories/CajaRepository.java
+++ src/main/java/com/two_weeks_backend/two_weeks_backend/repositories/CajaRepository.java
@@ -1,9 +1,10 @@
 package com.two_weeks_backend.two_weeks_backend.repositories;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.two_weeks_backend.two_weeks_backend.entities.CajaEntity;
 
 @Repository
-public interface CajaRepository extends JpaRepository<CajaEntity, Long> {}
+public interface CajaRepository extends JpaRepository<CajaEntity, Long> {
+}
--- src/main/java/com/two_weeks_backend/two_weeks_backend/services/CajaService.java
+++ src/main/java/com/two_weeks_backend/two_weeks_backend/services/CajaService.java
@@ -1,34 +1,32 @@
 package com.two_weeks_backend.two_weeks_backend.services;
 
-import com.two_weeks_backend.two_weeks_backend.entities.CajaEntity;
-import com.two_weeks_backend.two_weeks_backend.repositories.CajaRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.two_weeks_backend.two_weeks_backend.entities.CajaEntity;
+import com.two_weeks_backend.two_weeks_backend.repositories.CajaRepository;
 
 @Service
 public class CajaService {
     @Autowired
     private CajaRepository cajaRepository;
-    
+
     @Transactional(rollbackFor = Exception.class)
-    public CajaEntity update(CajaEntity caja) {
+    public void update(CajaEntity caja) {
         CajaEntity retrievedCaja = this.get(caja.getId());
-        
+
         retrievedCaja.checkIfPossibleUpdating();
         retrievedCaja.setMontoInicial(caja.getMontoInicial());
         retrievedCaja.calculateGanancia();
 
-        return this.cajaRepository.save(retrievedCaja);
+        this.cajaRepository.save(retrievedCaja);
     }
 
     public CajaEntity get(Long id) {
-        return this.cajaRepository
-            .findById(id)
-            .orElseThrow(() -> new RuntimeException("caja no encontrada"));
+        return this.cajaRepository.findById(id).orElseThrow(() -> new RuntimeException("caja no encontrada"));
     }
 
     public Page<CajaEntity> get(Pageable pageable) {
